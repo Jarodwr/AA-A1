9d42eca3aaf6ac428bcb4e35d6a42c42b7c63d46
Splerge <damnsplurge@gmail.com>
2017-04-06 13:47:19 +1000

    Completed data generation

--- src/generation/DataSetGen.java
+++ src/generation/DataSetGen.java
@@ -1,5 +1,42 @@
 package generation;
 
-public class DataSetGen {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
 
+public class DataSetGen {
+	
+	public static void main(String[] args) {
+		try {
+			generateDataSet(1000, "sampleDataGenerated.txt");
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static final Double[] topLeftCityBounds = {-37.656614, 144.759037};
+	public static final Double[] bottomRightCityBounds = {-38.188780, 145.284321};
+	public static final String[] categories = {"restaurant", "education", "hospital"};
+	/*
+	 * @numberOfRecords is how many points to generate
+	 * 
+	 */
+	public static void generateDataSet(int numberOfRecords, String outputFileName) throws FileNotFoundException {
+
+        File outputFile = new File(outputFileName);
+        PrintWriter writer = new PrintWriter(outputFile);
+        
+        Random r = new Random();
+        
+		for (int i = 0; i < numberOfRecords; i++) {
+			
+			double x = topLeftCityBounds[0] + (bottomRightCityBounds[0] - topLeftCityBounds[0]) * r.nextDouble();
+			double y = topLeftCityBounds[1] + (bottomRightCityBounds[1] - topLeftCityBounds[1]) * r.nextDouble();
+			String cat = categories[r.nextInt(3)];
+			writer.println("id" + i + " " + cat + " " + x + y);
+			
+		}
+	}
 }
