007b5c5220948e473cac22e39355ae3b30a15db7
Splerge <damnsplurge@gmail.com>
2017-04-15 15:32:41 +1000

    added Generation class for generating all at the same time

---
+++ src/generation/Generation.java
@@ -0,0 +1,47 @@
+package generation;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import nearestNeigh.Category;
+import nearestNeigh.Point;
+
+public class Generation {
+	public static void main(String[] args) {
+		try {
+			String smallData ="smallDataSet.txt";
+			String mediumData = "mediumDataSet.txt";
+			String largeData = "LargeDataSet.txt";
+			DataSetGen.generateDataSet(1000, smallData);
+			DataSetGen.generateDataSet(2500, mediumData);
+			DataSetGen.generateDataSet(7000, largeData);
+
+			CommandSetGen.testAdditionRemovalEffect(txtFileIntoList(smallData), "smalldataset_additionremovaltest");
+			CommandSetGen.testAdditionRemovalEffect(txtFileIntoList(mediumData), "mediumdataset_additionremovaltest");
+			CommandSetGen.testAdditionRemovalEffect(txtFileIntoList(largeData), "largedataset_additionremovaltest");
+		
+			CommandSetGen.testVarianceOfKValue(4, "kvariancetest");
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("some IO error");
+		}
+	}
+	
+	public static List<Point> txtFileIntoList(String filename) throws FileNotFoundException {
+		List<Point> points = new ArrayList<Point>();
+        File dataFile = new File(filename);
+        Scanner scanner = new Scanner(dataFile);
+        while (scanner.hasNext()) {
+            String id = scanner.next();
+            Category cat = Point.parseCat(scanner.next());
+            Point point = new Point(id, cat, scanner.nextDouble(), scanner.nextDouble());
+            points.add(point);
+        }
+            scanner.close();
+            return points;
+	}
+}
